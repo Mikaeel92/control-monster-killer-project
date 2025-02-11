dd1a11399c8429093a1d5a46682803d1cb1f1c62
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-02-11 10:15:12 +0330

    heal functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 const ATTACK_VALUE = 10
 const MONSTER_ATTACK_VALUE = 14
 const STRONG_ATTACK_VALUE = 17
+const HEAL_VALUE = 20
 
 let chosenMaxLife = 100
 let currentMonstersHealth = chosenMaxLife
@@ -8,16 +9,8 @@ let currentPlayerHealth = chosenMaxLife
 
 adjustHealthBars(chosenMaxLife)
 
-function attackMonster (mode) {
-let maxDamage
-if(mode === 'ATTACK') {
-    maxDamage = ATTACK_VALUE
-} else if (mode === 'STRONG_ATTACK'){
-    maxDamage = STRONG_ATTACK_VALUE
-}
-const damage = dealMonsterDamage(maxDamage)
-currentMonstersHealth -= damage
-const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
+function endRound () {
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
 currentPlayerHealth -= playerDamage
 if(currentMonstersHealth <= 0 && currentPlayerHealth >= 0) {
     alert('You Won!')
@@ -28,6 +21,18 @@ if(currentMonstersHealth <= 0 && currentPlayerHealth >= 0) {
 }
 }
 
+function attackMonster (mode) {
+let maxDamage
+if(mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE
+} else if (mode === 'STRONG_ATTACK'){
+    maxDamage = STRONG_ATTACK_VALUE
+}
+const damage = dealMonsterDamage(maxDamage)
+currentMonstersHealth -= damage
+endRound()
+}
+
 function attackHandler () {
     attackMonster('ATTACK')
 }
@@ -36,5 +41,20 @@ function strongAttackHandler () {
     attackMonster('STRONG_ATTACK')
 }
 
+function healPlayerHandler () {
+    let healValue
+    if(currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+        alert("You Can't Heal To More Than Your Initial Health!")
+        healValue = chosenMaxLife - currentPlayerHealth
+    } else {
+        healValue = HEAL_VALUE
+    }
+
+    increasePlayerHealth(healValue)
+    currentPlayerHealth += healValue
+    endRound()
+}
+
 attackBtn.addEventListener('click', attackHandler)
 strongAttackBtn.addEventListener('click', strongAttackHandler)
+healBtn.addEventListener('click', healPlayerHandler)
