0f4ec8e73d0bb580ae69a618e0aebd11e06b772a
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-02-13 11:32:24 +0330

    for of
    in for of loop we can't access to the index..
    we should do it by ourselfe

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -201,9 +201,11 @@ function printLogHandler () {
     // for (let i = 0; i < battleLog.length; i++) {
     //     console.log(battleLog[i])
     // }
-
+    let i = 0
     for (const logEntry of battleLog) {
         console.log(logEntry)
+        console.log(i)
+        i++
     }
 }
 
